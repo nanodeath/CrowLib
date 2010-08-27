90ea485270bb09e4fc710c29acecb580f75be6d0
Max Aller <nanodeath@gmail.com>
2010-08-26 23:50:46 -0700

    "same" is not always defined...

--- src/crow/util/Test.js
+++ src/crow/util/Test.js
@@ -1,11 +1,13 @@
 goog.provide('crow.util.Test');
 
-// Wrap the regular same function, because otherwise _cachedHash trips up some of the tests.
-var oldSame = same;
-var newSame = function(){
-	for(var i = 0; i < arguments.length; i++){
-		delete arguments[i]._cachedHash;
+if(typeof same !== "undefined"){
+	// Wrap the regular same function, because otherwise _cachedHash trips up some of the tests.
+	var oldSame = same;
+	var newSame = function(){
+		for(var i = 0; i < arguments.length; i++){
+			delete arguments[i]._cachedHash;
+		}
+		return oldSame.apply(this, arguments);
 	}
-	return oldSame.apply(this, arguments);
+	same = newSame;
 }
-same = newSame;
