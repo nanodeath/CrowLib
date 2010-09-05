88bea56975770b218602a63d078a62efd69c260e
Max Aller <nanodeath@gmail.com>
2010-09-05 11:33:24 -0700

    Modified base node so it's not abstract and has reasonable defaults for parameters.

--- src/crow/BaseNode.js
+++ src/crow/BaseNode.js
@@ -3,20 +3,26 @@ goog.provide('crow.BaseNode');
 /**
  * @constructor
  */
-crow.BaseNode = function(){};
+crow.BaseNode = function(arr){
+	if(arr){
+		this.x = arr[0];
+		this.y = arr[1];
+	}
+};
 
 // Override these position methods in your base class to provide location
-// information
-crow.BaseNode.prototype.getX = function(){ throw new Error("override me") };
-crow.BaseNode.prototype.getY = function(){ throw new Error("override me") };
+// information.  By default, getX and getY return the 'x' and 'y' properties of this node.
+crow.BaseNode.prototype.getX = function(){ return this.x; };
+crow.BaseNode.prototype.getY = function(){ return this.y; };
 
 // Calculating distance between nodes.	You have several options:
 // 1) Simply override the distanceAlgorithm method in your derived class with
-//	 a) one of the GraphUtil.distance.* methods, or
+//	 a) one of the crow.GraphUtil.distance.* methods, or
 //	 b) your own method that takes a dx and a dy
+//   c) leave it at the default (which is manhattan distance)
 // 2) Override the distanceTo method in your derived class to provide completely
 //	 custom behavior 
-crow.BaseNode.prototype.distanceAlgorithm = function(){ throw new Error("override me with a GraphUtil.distance.* method"); };
+crow.BaseNode.prototype.distanceAlgorithm = crow.GraphUtil.distance.manhattan;
 crow.BaseNode.prototype.distanceTo = function(other){
 	var dx = this.x - other.x,
 		dy = this.y - other.y;
@@ -24,14 +30,9 @@ crow.BaseNode.prototype.distanceTo = function(other){
 	return this.distanceAlgorithm(dx, dy);
 };
 
-crow.BaseNode.prototype.hash = function(clear_cache){
-		var h = this._cachedHash;
-		if(!h || clear_cache){
-			h = this.x + "_" + this.y;
-			this._cachedHash = h;
-		}
-		return h;
-	}
+crow.BaseNode.prototype.hash = function(){
+	return this.x + "_" + this.y;
+}
 
 // Find neighbors of this node in the provided graph
 // (checks horizontally and vertically, not diagonally)
